2e9f555962f59b59ac425e9b9274ff4044a77581
Morgan <dev@morgansjordan.com>
2022-05-15 20:41:45 +0200

    adding htaccess

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo "[+] Switching to branch master"
-git checkout master
+echo "[+] Switching to branch main"
+git checkout main
 
 echo "[+] Building app"
 npm run build
 
 echo "[+] Deploying files to server"
-rsync -avP build/ morgan@http://139.162.139.191:/var/www/morgansjordan.com/
+rsync -avP -e "ssh -i $HOME/.ssh/id_rsamorgansjordan" build/ morgan@139.162.139.191:/var/www/morgansjordan.com/
 echo "Deployment complete"
